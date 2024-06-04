1460f7fd9fe56e91e9c0e0342205141fbf8e5358
Rahmat-Khavari-Hyper-Island <145808562+Rahmat-Khavari-Hyper-Island@users.noreply.github.com>
2024-06-04 17:59:49 +0200

    add the a few changes + the audio button

--- script.js
+++ script.js
@@ -1,52 +1,28 @@
-// script.js
 let audioContext;
 let audioElement;
 
-// Function to create audio context and element and play audio
-function startAudio() {
-  // Create audio context
+const audioBtn = document.querySelector(".audio-btn");
+let isPlaying = false;
+
+function createAudio() {
   audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-  // Attempt to resume the audio context
-  if (audioContext.state === "suspended") {
-    audioContext
-      .resume()
-      .then(() => {
-        console.log("Audio context resumed successfully");
-      })
-      .catch((error) => {
-        console.log("Error resuming audio context:", error);
-      });
-  }
-
-  // Create audio element
   audioElement = new Audio("audio/bg-audio.m4a");
   const source = audioContext.createMediaElementSource(audioElement);
   source.connect(audioContext.destination);
 
-  // Set volume to half
-  audioElement.volume = 0.4;
-
-  // Enable looping
+  audioElement.volume = 0.7;
   audioElement.loop = true;
-
-  // Play audio
-  audioElement
-    .play()
-    .then(() => {
-      console.log("Audio playback started successfully");
-    })
-    .catch(function (error) {
-      console.log("Error playing audio:", error);
-    });
-
-  // Remove event listener to prevent multiple audio playbacks
-  document.body.removeEventListener("mouseover", startAudio);
 }
 
-// Start audio playback when the user hovers over the body of the page
-document.body.addEventListener("mouseover", startAudio);
+createAudio();
 
-window.onload = function () {
-  window.scrollTo(0, 0);
-};
+audioBtn.addEventListener("click", function () {
+  if (isPlaying) {
+    audioBtn.textContent = "play audio";
+    audioElement.pause();
+  } else {
+    audioBtn.textContent = "pause audio";
+    audioElement.play();
+  }
+  isPlaying = !isPlaying;
+});
